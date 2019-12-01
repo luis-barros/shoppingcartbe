7d515446516daf8804c4cbf3ad004a8d0c42a437
luis <luigi.barros@gmail.com>
2019-12-01 12:01:12 +0100

    commit

--- server.js
+++ server.js
@@ -1,16 +1,34 @@
+const express = require('express');
+const oDataServer = require('simple-odata-server');
+const Adapter = require('simple-odata-server-mongodb')
+const app = express();
+const port = 3000;
+const url = 'mongodb://localhost:27017'
 const mongo = require('mongodb').MongoClient;
-const url = 'mongodb://localhost:27017';
 
-mongo.connect(url, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}, (err, client) => {
-  if(err) {
-    console.error(err);
-    return;
-  }
-  const db = client.db('test');
-  const collection = db.collection('list');
-  collection.insertOne({name: 'apple'});
-  client.close();
-});
+var model = {
+    namespace: "test",
+    entityTypes: {
+        "listType": {
+            "_id": {"type": "Edm.String", key: true},
+            "name": {"type": "Edm.String"},
+        }
+    },
+    entitySets: {
+        "list": {
+            entityType: "listType"
+        }
+    }
+};
+
+var odataServer = oDataServer().model(model);
+
+mongo.connect(url, function(err, db) {
+  odataServer.adapter(Adapter(function(cb) {
+    cb(err, db.db('test'));
+  }));
+})
+app.use("/odata", function(req, res) {
+  odataServer.handle(req, res);
+})
+app.listen(port, () => console.log('listening on port ${port}'))
